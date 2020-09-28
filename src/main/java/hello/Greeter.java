11f009b4c52cd9e24f73ac577089d9949c2b7d6b
pavansw <pavanwankhade4u@gmail.com>
2020-09-28 12:06:19 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world pavan! This is for CICD demo, Bye ";
+    return "Hello world----- pavan is your Trainer ! This is for CICD demo, Bye ";
   }
 }
