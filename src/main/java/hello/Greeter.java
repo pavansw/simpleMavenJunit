b750c8b67496008da2a9d73a24a90094a59c8878
pavansw <pavanwankhade4u@gmail.com>
2021-01-20 12:54:59 +0530

    hiii replaced by Bye in print msg

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- pavan is your Trainer ! Hiiii This is for CICD demo ,Need string to pass the test so Hi";
+    return "Hello world----- pavan is your Trainer ! Bye This is for CICD demo ,Need string to pass the test so Hi";
   }
 }
