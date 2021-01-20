190d183da9ceb44351a6089397b4326393d913e6
pavansw <pavanwankhade4u@gmail.com>
2021-01-20 12:49:46 +0530

    removed target directory and HI replaced by Hiii

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world----- pavan is your Trainer ! Hi This is for CICD demo ,Need string to pass the test so Hi";
+    return "Hello world----- pavan is your Trainer ! Hiiii This is for CICD demo ,Need string to pass the test so Hi";
   }
 }
