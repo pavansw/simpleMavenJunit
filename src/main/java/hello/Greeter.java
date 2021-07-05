40109713a42d998fa02af38b30025967df1cf897
pavansw <pavanwankhade4u@gmail.com>
2021-07-05 11:50:49 +0000

    removed Bye from output

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Pavan Wankhade your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
+    return "Hello world- This is Pavan Wankhade your Trainer !! Hi or  is the string needed here.Testing Pipeline";
   }
 }
