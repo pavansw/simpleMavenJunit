1f15a151d261d795e3ef79628574629cd6612c31
pavansw <pavanwankhade4u@gmail.com>
2021-12-18 16:39:38 +0000

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Pavan Wankhade your Trainer !! Hi or  hello is the string needed here.";
+    return "Hello world- This is Pavan Wankhade your Trainer !! Hi or Bye hello is the string needed here.";
   }
 }
