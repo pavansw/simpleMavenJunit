46b8a1690fbd0536b8aa0c20efe6808d05ef6779
pavansw <pavanwankhade4u@gmail.com>
2022-09-20 23:10:00 -0700

    Code updated by Pavan

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
   @Test
   public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Bye"));
+    assertThat(greeter.sayHello(), containsString("Wankhade"));
   }
 
 }
