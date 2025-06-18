b95e6cdaba56ee432c1266b5a0422e440b4c7350
Pavan Satish Wankhade <pavanwankhade4u@gmail.com>
2025-06-18 16:43:35 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Pavan!! Hi This is Devops training. Lets have a demo";
+    return "Hello world- This is Pavan!! Hi This is Devops training. Lets have a demo Bye";
   }
 }
